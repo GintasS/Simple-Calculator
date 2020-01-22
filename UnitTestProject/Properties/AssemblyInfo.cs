ddfe42dd4597cb2afac24fa19bb1ae28eede5563
Gintasss <svedgintas@gmail.com>
2020-01-22 15:30:23 +0200

    Added unit tests.

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f418d52-3b2e-4e3c-8810-9ed5feb33e29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDivideByZero()
+        {
+            var calculation = "5 / 0";
+            var expected = Constants.Symbol.Infinity.ToString();
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestNaNValue()
+        {
+            var calculation = "0 / 0";
+            var expected = Constants.Symbol.NaN;
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSimpleCalculation()
+        {
+            var calculation = "5 + 6";
+            var expected = "11";
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSimpleCalculation2()
+        {
+            var calculation = "5 / 6";
+            var expected = "0.833333333333333";
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSimpleCalculation3()
+        {
+            var calculation = "5 * 6";
+            var expected = "30";
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSimpleCalculation4()
+        {
+            var calculation = "5 - 6";
+            var expected = "-1";
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSimpleCalculation5()
+        {
+            var calculation = "5.3*6.3";
+            var expected = "33.39";
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSimpleCalculation6()
+        {
+            var calculation = "-4 * 5";
+            var expected = "-20";
+            var actual = ResultsManager.Calculate(calculation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestConvertDisplaySymbols()
+        {
+            var calculatorInput = "55 ÷ 5 + 66 × 0";
+            var expected = "55 / 5 + 66 * 0";
+            var actual = ResultsManager.ConvertDisplaySymbols(calculatorInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestReplaceIllegalUserInput()
+        {
+            var calculatorInput = "55 + 6.";
+            var expected = "55 + 6.0";
+            var actual = UserInput.RemoveIllegalUserInput(calculatorInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+    }
+}
